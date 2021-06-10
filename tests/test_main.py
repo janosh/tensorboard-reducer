69bd07ed510b94b0ae1ddd5b06f77d67953b63a2
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-06-10 06:59:05 +0200

    more main tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,18 +5,51 @@ import pytest
 from tensorboard_reducer import main
 
 
+argv_strict = ["-i", "tests/runs/strict/run_*", "-o", "tmp/strict"]
+argv_lax = ["-i", "tests/runs/lax/run_*", "-o", "tmp/lax"]
+
+
 def test_main():
     """Test main()."""
 
     rmtree("tmp/strict-mean", ignore_errors=True)
 
-    main(argv=["-i", "tests/runs/strict/run_*", "-o", "tmp/strict"])
+    main(argv_strict)
 
     with pytest.raises(Exception) as exc_info:
-        main(argv=["-i", "tests/runs/strict/run_*", "-o", "tmp/strict"])
+        main(argv_strict)
 
     assert exc_info.errisinstance(FileExistsError), "Unexpected error instance"
 
 
 def test_main_overwrite():
-    main(argv=["-i", "tests/runs/strict/run_*", "-o", "tmp/strict", "-f"])
+    main(argv_strict + ["-f"])
+
+
+def test_main_multi_reduce():
+    main(argv_strict + ["-f", "-r", "mean,std,min,max"])
+
+
+def test_main_lax():
+    # make sure we start from clean slate in case prev test failed
+    rmtree("tmp/lax-mean", ignore_errors=True)
+
+    with pytest.raises(Exception) as exc_info:
+        main(argv_lax)
+
+    assert exc_info.errisinstance(AssertionError), "Unexpected error instance"
+
+    with pytest.raises(Exception) as exc_info:
+        main(argv_lax + ["--lax-tags"])
+
+    assert exc_info.errisinstance(AssertionError), "Unexpected error instance"
+
+    with pytest.raises(Exception) as exc_info:
+        main(argv_lax + ["--lax-steps"])
+
+    assert exc_info.errisinstance(AssertionError), "Unexpected error instance"
+
+    main(argv_lax + ["--lax-tags", "--lax-steps"])
+
+    # make sure outdir was created
+    rmtree("tmp/strict-mean")
