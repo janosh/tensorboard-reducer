6d3468610d2933a23bc355250f9c76e6b6bb0151
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-04-27 19:45:51 +0100

    refactor test_load_tb_events_strict() and test_load_tb_events_handle_dup_steps()

--- tests/test_load.py
+++ tests/test_load.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from glob import glob
 
-import numpy as np
 import pandas as pd
 import pytest
 
@@ -17,7 +16,6 @@ def test_load_tb_events_strict(events_dict: dict[str, pd.DataFrame]) -> None:
     Test load_tb_events for strict input data, i.e. without any of the special cases
     below. events_dict is just the output of load_tb_events() (see conftest.py).
     """
-
     actual_type = type(events_dict)
     assert_dict = f"return type of load_tb_events() is {actual_type}, expected dict"
     assert actual_type == dict, assert_dict
@@ -37,16 +35,19 @@ def test_load_tb_events_strict(events_dict: dict[str, pd.DataFrame]) -> None:
     assert_len = f"load_tb_events() returned {n_runs} TB runs, expected 3"
     assert n_steps == 100, assert_len
 
-    run_means = events_dict["strict/foo"].to_numpy().mean()
-    assert_means = f"load_tb_events() data has unexpected mean {run_means:.5}"
-    assert np.allclose(run_means, 2.476, atol=1e-3), assert_means
+    # columns correspond to different runs for the same tag, the mean across a run is
+    # meaningless and only used for asserting value constancy
+    # sorted since order depends on filesystem and is different on Windows
+    column_means = sorted(events_dict["strict/foo"].mean(0))
+
+    assert_means = f"load_tb_events() data has unexpected mean {column_means}"
+    assert column_means == pytest.approx([1.488, 2.459, 3.481], abs=1e-3), assert_means
 
 
 def test_load_tb_events_lax_tags() -> None:
     """Ensure load_tb_events throws an error on runs with different step counts when not
     setting strict_steps=False.
     """
-
     with pytest.raises(AssertionError, match="Unequal number of steps"):
         load_tb_events(lax_runs, strict_tags=False)
 
@@ -55,7 +56,6 @@ def test_load_tb_events_lax_steps() -> None:
     """Ensure load_tb_events throws an error on runs with different sets of tags when
     not setting strict_tags=False.
     """
-
     with pytest.raises(AssertionError, match="Some tags appear only in some logs"):
         load_tb_events(lax_runs, strict_steps=False)
 
@@ -65,7 +65,6 @@ def test_load_tb_events_lax_tags_and_steps() -> None:
     Test loading TensorBoard event files when both different sets of tags and
     different step counts across runs should not throw errors.
     """
-
     events_dict = load_tb_events(lax_runs, strict_tags=False, strict_steps=False)
 
     tags_list = ["lax/bar_1", "lax/bar_2", "lax/bar_3", "lax/bar_4", "lax/foo"]
@@ -82,7 +81,6 @@ def test_load_tb_events_handle_dup_steps() -> None:
     Test loading TensorBoard event files with duplicate steps, i.e. multiple values
     for the same tag at the same step. See handle_dup_steps kwarg.
     """
-
     with pytest.raises(AssertionError, match="contains duplicate steps"):
         load_tb_events(dup_steps_runs)
 
@@ -94,11 +92,11 @@ def test_load_tb_events_handle_dup_steps() -> None:
         kept_first_dups.keys() == kept_last_dups.keys() == mean_dups.keys()
     ), "key mismatch between first, last and mean duplicate handling"
 
-    first_df, last_df, mean_df = (
+    df_first, df_last, df_mean = (
         dic["dup_steps/foo"] for dic in [kept_first_dups, kept_last_dups, mean_dups]
     )
 
-    assert np.allclose((first_df + last_df) / 2, mean_df, atol=1e-3), (
+    pd.testing.assert_frame_equal((df_first + df_last) / 2, df_mean), (
         "taking the average of keeping first and last duplicates gave different result "
         "than taking the mean of duplicate steps"
     )
