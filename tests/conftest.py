4cdba7ead7b3ff4dee557497ea72cd14cc2fbf17
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-07-06 12:17:16 +0100

    Add support for writing reduction results to Excel (#23)
    * add excel support to write_data_file()
    
    * document excel support in readme
    
    * update test_write_data_file() to test .xls and .xlsx extension
    
    * add test_write_data_file_with_bad_ext()
    
    * add extras_require to setup.cfg and use it in CI install step
    
    * rewrite tests to use pytest tmp_path fixture instead of local dir ./tmp
    
    updated tests:
    test_main_lax_csv_output()
    test_write_tb_events()
    test_write_data_file()
    
    * replace tmpdir: py.path.local fixture with tmp_path: Path in test_main.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,29 @@
 from __future__ import annotations
 
-import os
 from glob import glob
 
 import pandas as pd
 import pytest
 
-from tensorboard_reducer import load_tb_events
+import tensorboard_reducer as tbr
+
+REDUCE_OPS = ("mean", "std", "median")
 
 
-# https://docs.pytest.org/en/6.2.x/fixture.html#scope-sharing-fixtures-across-classes-modules-packages-or-session
-# load events_dict once and reuse across test files for speed
-@pytest.fixture(scope="module")
+# load events_dict once and reuse across tests
+# https://docs.pytest.org/en/6.2.x/fixture.html#fixture-scopes
+@pytest.fixture(scope="session")
 def events_dict() -> dict[str, pd.DataFrame]:
-    os.makedirs("tmp", exist_ok=True)
-
-    tb_events_dict = load_tb_events(glob("tests/runs/strict/run_*"))
+    tb_events_dict = tbr.load_tb_events(glob("tests/runs/strict/run_*"))
 
     return tb_events_dict
+
+
+@pytest.fixture(scope="session")
+def reduced_events(
+    events_dict: dict[str, pd.DataFrame]
+) -> dict[str, dict[str, pd.DataFrame]]:
+
+    reduced_events = tbr.reduce_events(events_dict, REDUCE_OPS)
+
+    return reduced_events
