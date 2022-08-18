59769ff49fa1b033f2d83c0dfea48bb6a48a602d
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-08-17 17:24:51 -0700

    Add `examples/wandb_integration.ipynb` (#26)
    * add nbQA and nbstripout commit hooks
    
    * tweak notebook descriptions and pre-commit run --all-files
    
    * readme add table with binder and github links to notebooks
    
    * change relative pkg imports to absolute
    
    * add examples/wandb_integration.ipynb
    
    * don't insert '-' between out_dir and reduce op when out_dir ends in / or \
    
    * tweak readme examples table

--- tensorboard_reducer/main.py
+++ tensorboard_reducer/main.py
@@ -6,17 +6,18 @@ from typing import Sequence
 
 import pandas as pd
 
-from .load import load_tb_events
-from .write import write_data_file, write_tb_events
+from tensorboard_reducer.load import load_tb_events
+from tensorboard_reducer.write import write_data_file, write_tb_events
 
 
 def reduce_events(
     events_dict: dict[str, pd.DataFrame], reduce_ops: Sequence[str]
 ) -> dict[str, dict[str, pd.DataFrame]]:
-    """Perform numpy reduce ops on the last axis of each array in a dictionary of
-    scalar TensorBoard event data. Each array enters
-    this function with shape (n_timesteps, r_runs) and len(reduce_ops) exit
-    with shape (n_timesteps,).
+    """Perform numpy reduce operations along the last dimension of each array in a
+    dictionary of scalar TensorBoard event data. Each array (1 per run) enters
+    this function with shape (n_steps, n_runs) and it returns a dict of len(reduce_ops)
+    subdicts each with keys named after scalar quantities (loss, accuracy, etc.)
+    holding arrays with shape (n_steps,).
 
     Args:
         events_dict (dict[str, pd.DataFrame]): Dict of arrays to reduce.
