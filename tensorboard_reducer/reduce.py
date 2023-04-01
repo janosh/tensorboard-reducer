91ecad996c74013f088002cb56320b5a66ee81d2
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-04-01 09:09:23 -0700

    DRY workflows (#33)
    * DRY workflows
    
    * tweak readme note re tensorboard-aggregator
    
    * move reduce_events() to new module reduce.py
    
    * ruff ignore flake8-eradicate
    
    * add tests 'test_reduce_events_dimensions' and 'test_reduce_events_empty_input'

---
+++ tensorboard_reducer/reduce.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from typing import Sequence
+
+import pandas as pd
+
+
+def reduce_events(
+    events_dict: dict[str, pd.DataFrame],
+    reduce_ops: Sequence[str],
+    verbose: bool = False,
+) -> dict[str, dict[str, pd.DataFrame]]:
+    """Perform numpy reduce operations along the last dimension of each array in a
+    dictionary of scalar TensorBoard event data. Each array (1 per run) enters this
+    function with shape (n_steps, n_runs) and it returns a dict of len(reduce_ops)
+    subdicts each with keys named after scalar quantities (loss, accuracy, etc.) holding
+    arrays with shape (n_steps,).
+
+    Args:
+        events_dict (dict[str, pd.DataFrame]): Dict of arrays to reduce.
+        reduce_ops (list[str]): Names of numpy reduce ops. E.g. mean, std, min, max, ...
+        verbose (bool, optional): Whether to print progress. Defaults to False.
+
+    Returns:
+        dict[str, dict[str, pd.DataFrame]]: Dict of dicts where each subdict holds one
+            reduced array for each of the specified reduce ops, e.g.
+            {"loss": {"mean": arr.mean(-1), "std": arr.std(-1)}}.
+    """
+    reductions: dict[str, dict[str, pd.DataFrame]] = {}
+
+    for op in reduce_ops:
+        reductions[op] = {}
+
+        for tag, df in events_dict.items():
+            reductions[op][tag] = getattr(df, op)(axis=1)
+
+    if verbose:
+        print(
+            f"Reduced {len(events_dict)} scalars with {len(reduce_ops)} operations:"
+            f" ({', '.join(reduce_ops)})"
+        )
+    return reductions
--- tests/test_reduce.py
+++ tests/test_reduce.py
@@ -1,18 +1,33 @@
 from __future__ import annotations
 
+from typing import Sequence
+
+import numpy as np
 import pandas as pd
 import pytest
 
 from tensorboard_reducer import reduce_events
 
 
+def generate_sample_data(
+    n_tags: int = 1, n_runs: int = 10, n_steps: int = 5
+) -> dict[str, pd.DataFrame]:
+    events_dict = {}
+    for idx in range(n_tags):
+        data = np.random.random((n_steps, n_runs))
+        df = pd.DataFrame(data, columns=[f"run_{j}" for j in range(n_runs)])
+        events_dict[f"tag_{idx}"] = df
+    return events_dict
+
+
 @pytest.mark.parametrize("verbose", [True, False])
+@pytest.mark.parametrize("reduce_ops", [["mean"], ["max", "min"], ["std", "median"]])
 def test_reduce_events(
     events_dict: dict[str, pd.DataFrame],
+    reduce_ops: Sequence[str],
     verbose: bool,
     capsys: pytest.CaptureFixture[str],
 ) -> None:
-    reduce_ops = ["mean", "std", "max", "min"]
     reduced_events = reduce_events(events_dict, reduce_ops, verbose=verbose)
 
     out_keys = list(reduced_events)
@@ -35,13 +50,14 @@ def test_reduce_events(
 
             assert (
                 n_steps == out_steps
-            ), f"length mismatch in initial ({n_steps}) and reduced ({out_steps}) steps"
+            ), f"length mismatch in initial={n_steps} and reduced={out_steps} steps"
 
-    min_data = reduced_events["min"]["strict/foo"]
-    max_data = reduced_events["max"]["strict/foo"]
-    assert all(
-        min_data <= max_data
-    ), "min reduction was not <= max reduction at every step"
+    if {"min", "max"} <= set(reduce_ops):
+        min_data = reduced_events["min"]["strict/foo"]
+        max_data = reduced_events["max"]["strict/foo"]
+        assert all(
+            min_data <= max_data
+        ), "min reduction was not <= max reduction at every step"
 
     stdout, stderr = capsys.readouterr()
     assert stderr == ""
@@ -52,3 +68,28 @@ def test_reduce_events(
         ) in stdout
     else:
         assert stdout == ""
+
+
+@pytest.mark.parametrize("n_tags, n_runs, n_steps", [(1, 10, 5), (2, 5, 3), (3, 3, 10)])
+def test_reduce_events_dimensions(n_tags: int, n_runs: int, n_steps: int) -> None:
+    events_dict = generate_sample_data(n_tags=n_tags, n_runs=n_runs, n_steps=n_steps)
+    reduce_ops = ["mean", "std", "max", "min"]
+    reduced_events = reduce_events(events_dict, reduce_ops)
+
+    for op, reduced_dict in reduced_events.items():
+        assert len(reduced_dict) == n_tags, (
+            f"mismatch in number of tags for operation {op}: "
+            f"expected {n_tags}, found {len(reduced_dict)}"
+        )
+
+        for tag, reduced_df in reduced_dict.items():
+            assert len(reduced_df) == n_steps, (
+                f"length mismatch in initial={n_steps} vs reduced={len(reduced_df)}"
+                f" steps for tag {tag} and operation {op}"
+            )
+
+
+@pytest.mark.parametrize("reduce_ops", [["mean"], ["max", "min"], ["std", "median"]])
+def test_reduce_events_empty_input(reduce_ops: Sequence[str]) -> None:
+    reduced_events = reduce_events({}, reduce_ops)
+    assert reduced_events == dict.fromkeys(reduce_ops, {})
