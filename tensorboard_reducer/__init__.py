73cbb10a0858cfee73112eaece86cdefe645cfa3
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-08-16 09:54:09 -0700

    add __version__ to __init__.py

--- tensorboard_reducer/__init__.py
+++ tensorboard_reducer/__init__.py
@@ -1,6 +1,16 @@
-from tensorboard_reducer.load import load_tb_events as load_tb_events
-from tensorboard_reducer.main import main as main
-from tensorboard_reducer.reduce import reduce_events as reduce_events
-from tensorboard_reducer.write import write_data_file as write_data_file
-from tensorboard_reducer.write import write_df as write_df
-from tensorboard_reducer.write import write_tb_events as write_tb_events
+"""TensorBoard Reducer package.
+
+Author: Janosh Riebesell (2021-04-04)
+"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+from tensorboard_reducer.load import load_tb_events
+from tensorboard_reducer.main import main
+from tensorboard_reducer.reduce import reduce_events
+from tensorboard_reducer.write import write_data_file, write_df, write_tb_events
+
+try:  # noqa: SIM105
+    __version__ = version("tensorboard-reducer")
+except PackageNotFoundError:
+    pass  # package not installed
